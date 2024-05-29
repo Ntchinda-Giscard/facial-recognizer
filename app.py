4bd3680df190d8f82910cbb21637344f4af02fbc
Ntchinda-Giscard <ntchinda1998@gmail.com>
2024-05-29 22:11:59 +0100

    add id to end point

--- app.py
+++ app.py
@@ -141,7 +141,7 @@ async def read_items():
 #     return JSONResponse(content={"message": f"Image {image.filename} saved successfully and name '{name}' received."})
 
 @app.post("/add-user")
-async def add_user(image: UploadFile = File(...), name: str = Form(...), location_id: int = Form(...)):
+async def add_user(image: UploadFile = File(...), name: str = Form(...), id: int = Form(...), location_id: int = Form(...)):
 
     try:
         image_path = os.path.join(UPLOAD_DIRECTORY, image.filename)
