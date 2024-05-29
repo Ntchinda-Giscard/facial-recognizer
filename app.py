266407369ad2dd73273c60820e1801d1f11e36e1
Ntchinda-Giscard <ntchinda1998@gmail.com>
2024-05-30 00:51:35 +0100

    remove score from result

--- app.py
+++ app.py
@@ -190,9 +190,9 @@ async def recognize(image: UploadFile = File(...)):
         result_data = lookup_user(index, encoding_list)
 
         if(result_data["matches"][0]["score"] >= 0.7900):
-            return JSONResponse(content={"message": "User found", "data": result_data, "status_code" : 200, "score": result_data["matches"][0]["score"]})
+            return JSONResponse(content={"message": "User found", "data": result_data, "status_code" : 200})
         else:
-            return JSONResponse(content={"message": "User not found", "status_code" : 404, "data": result_data, "score": result_data["matches"][0]["score"]})
+            return JSONResponse(content={"message": "User not found", "status_code" : 404, "data": result_data})
         
     except Exception as e:
         return JSONResponse(content={"message": f"Internal server error {str(e)}", "status_code": 500})
