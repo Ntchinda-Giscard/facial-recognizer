715394b2f7a6d042261f82e368f9382f6bc66280
Ntchinda-Giscard <ntchinda1998@gmail.com>
2024-05-29 14:00:47 +0100

    deppface

--- test.py
+++ test.py
@@ -1,18 +1,28 @@
+
+
+
+import uuid
 import requests
 
 # Define the endpoint URL
 url = "https://ntchinda-giscard-facial-reg.hf.space/recognize"
 
-# Define the path to the image file you want to upload
-file_path = "francisman.jpeg"
+# Define the file and data to be sent
+file_path = "fran2.jpeg"
+name = "Francis"
+id = str(uuid.uuid4())
 
 # Open the file in binary mode
 with open(file_path, 'rb') as file:
     # Create a dictionary with the form data
     files = {'image': file}
+    data = {'name': name, 'id': id}
     
     # Send the POST request
-    response = requests.post(url, files=files)
+    response = requests.post(url, 
+                             files=files, 
+                            #  data=data
+                             )
     
     # Print the response from the server
     print(response.status_code)
